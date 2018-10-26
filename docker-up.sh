5d308079e019b5cec07977c4b91611549eb7f2f2
mrclemrkz <awesomeisme007@gmail.com>
2018-10-26 18:24:24 +0530

    adding copper users to openldap

--- docker-up.sh
+++ docker-up.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-docker-compose up -d --build
+docker-compose up --build -d
 
 STATUS="0"
 
 until [[ ${STATUS} == *"ok"* ]]; do
     echo "   wating until slpd is started"
-    STATUS=`docker exec -ti openldap sh -c "service slapd status"`
+    STATUS=`docker exec -ti copper-openldap sh -c "service slapd status"`
     sleep 5
 done
 
 echo "   slapd has started!"
 
-docker exec -it openldap /usr/bin/ldapadd -Y EXTERNAL -H ldapi:// -f /home/97-wso2Person.ldif
-docker exec -it openldap /usr/bin/ldapadd -Y EXTERNAL -H ldapi:// -f /home/98-scimPerson.ldif
-docker exec -it openldap /usr/bin/ldapadd -Y EXTERNAL -H ldapi:// -f /home/99-identityPerson.ldif
+docker exec -it copper-openldap /usr/bin/ldapadd -Y EXTERNAL -H ldapi:// -f /home/97-wso2Person.ldif
+docker exec -it copper-openldap /usr/bin/ldapadd -Y EXTERNAL -H ldapi:// -f /home/98-scimPerson.ldif
+docker exec -it copper-openldap /usr/bin/ldapadd -Y EXTERNAL -H ldapi:// -f /home/99-identityPerson.ldif
 
-docker exec -it openldap /usr/bin/ldapadd -x -D 'cn=admin,dc=copper,dc=opensource,dc=lk' -w admin -H ldapi:// -f /home/add-ou.ldif
+# docker exec -it copper-openldap /usr/bin/ldapadd -x -D 'cn=admin,dc=copper,dc=opensource,dc=lk' -w admin -H ldapi:// -f /home/add-ou.ldif
+docker exec -it copper-openldap /usr/bin/ldapadd -x -D 'cn=admin,dc=copper,dc=opensource,dc=lk' -w admin -H ldapi:// -f /home/copper-users.ldif
